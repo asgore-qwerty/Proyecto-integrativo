95bda279b96691dcd9cf96e3342f4d4edb411719
stivengilperez-source <stivengilperez@gmail.com>
2026-04-24 11:40:56 -0500

    administradoresDb

---
+++ src/db/administradoresDB.js
@@ -0,0 +1,31 @@
+const administradoresDB = [
+  {
+    id: 1,
+    nombre: "Carolina Ortis",
+    contrasena: "1234",
+    estado: "Activo",
+    
+    
+  },
+  {
+    id: 2,
+    nombre: "Mauricio Castaño",
+    contrasena: "1234",
+    estado: "Inactivo",
+    
+    
+  },
+  {
+    id: 3,
+    nombre: "felipe Gaviria",
+    contrasena: "1234",
+    estado: "Activo",
+  }
+  {
+    id: 4,
+    nombre: "jonatan Gil",
+    contrasena: "1234",
+    estado: "Activo",
+    
+  }
+];
