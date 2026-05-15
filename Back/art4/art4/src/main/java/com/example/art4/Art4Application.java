3a6466b4b5dfacc224fa9f9b052880fd05b0727b
elmontacabras <mosqueranatalia422@gmail.com>
2026-05-15 10:31:21 -0500

    Reestructuración de carpetas en el backend

---
+++ Back/art4/art4/src/main/java/com/example/art4/Art4Application.java
@@ -0,0 +1,13 @@
+package com.example.art4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Art4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Art4Application.class, args);
+	}
+
+}
---
+++ Back/art4/art4/src/test/java/com/example/art4/Art4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.art4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Art4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
