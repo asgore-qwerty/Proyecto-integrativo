a6a7e362dba937d43ec1aed6e78e1d795470b88c
Maria alejandra Tamayo <zarachoe123@gmail.com>
2026-05-22 10:26:16 -0500

    Actualización back obras

---
+++ Back/art4/art4/src/main/java/com/example/art4/servicios/ObraServicio.java
@@ -0,0 +1,17 @@
+package com.example.art4.servicios;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.art4.modelos.Obra;
+import com.example.art4.repositorios.IObraRepositorio;
+
+@Service
+public class ObraServicio {
+
+    @Autowired
+    private IObraRepositorio repositorio;
+
+    public Obra 
+
+}
